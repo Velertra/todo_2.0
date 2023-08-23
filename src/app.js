07973e96fcb87572aefb287efafe7ec5bae5a8c0
Velertra <75776124+Velertra@users.noreply.github.com>
2023-08-22 23:39:49 -0700

    set function that collects task info

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('just checking');
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.tile = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+const task1 = new Task("Complete Project", "Finish coding the project", "2023-08-31", "High");
+
+console.log(task1);

bdd8b1e8917cf08387bde0c8c57af34b573cc3ae
Velertra <75776124+Velertra@users.noreply.github.com>
2023-08-28 23:59:51 -0700

    rearranged the class function to create the array instead of taking in information.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,4 @@
-class toDoTasks {
-    constructor(title, description, dueDate, priority) {
-        this.tile = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-}
-
+/* 
 const toDoTasks1 = new toDoTasks("Complete Project", "Finish coding the project", "2023-08-31", "High");
 
 console.log(toDoTasks1); 
@@ -19,8 +11,6 @@ class Projects {
     }
 }
 
-const projectList = new Projects('one', 'two', 'three', 'four');
-
 console.log(projectList);
 
 
@@ -32,6 +22,80 @@ function createBtn(domLocation, ...para){
         domLocation.appendChild(btn);
     }
 }
-
+ */
 
 //createBtn(document.body, 'one', 'two', 'three');
+
+//todolist.js // saves form information to local storage while creating array for list. then parses information
+class TodoList {
+    constructor() {
+        this.task = [];
+    }
+    saveData(task) {
+        this.task.push(task)
+        localStorage.setItem('task', JSON.stringify(this.task));
+    }
+    loadSavedData() {
+        const taskString = localStorage.getItem('task');
+        if(taskString) {
+            this.task = JSON.parse(taskString);
+        }
+    }
+}
+
+//form EventListener, form values. sends to array.
+document.addEventListener('DOMContentLoaded', () => {
+    const todoListSection = new TodoList();
+    
+
+    todoListSection.loadSavedData();
+    
+    const form = document.getElementById('task_form');
+    const taskContainer = document.getElementById('task_container');
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const title = document.getElementById('title').value;
+        const description = document.getElementById('description').value;
+        const dueDate = document.getElementById('due_date').value;
+        const priority = document.getElementById('priority').value;
+        const notes = document.getElementById('notes').value;
+
+        const task = {
+            title,
+            description,
+            dueDate,
+            priority,
+            notes
+        };
+
+        todoListSection.saveData(task);
+
+        form.reset();
+    });
+    const renderButton = document.getElementById('home_btn');
+    renderButton.addEventListener('click', () => {
+        renderAllTasks(todoListSection.task, taskContainer)
+    });
+
+    function renderAllTasks(tasks, container) {
+        container.innerHTML = '';
+        tasks.forEach(task => {
+            renderTask(task, container);
+        });
+    }
+
+    function renderTask(task, container) {
+        const taskItem = document.createElement('div');
+        taskItem.className = 'task-item';
+        taskItem.innerHTML = `
+        <h2>${task.title}</h2>
+        <p>${task.description}</p>`
+        ;
+    container.appendChild(taskItem);
+    }
+});
+
+console.log(localStorage.task);
+
